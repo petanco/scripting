0024593c35cde75985db98603efe98ace51aafa2
petanco <57901348+petanco@users.noreply.github.com>
2019-11-18 14:18:38 +0100

    Update leandro.sh

--- leandro.sh
+++ leandro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #DESCRIPTION script basico
 #AUTHOR leandro galipolo
-dialog --title "Hola" --msgbox 'Hola Sololinux!' 6 20
+dialog --menu "Programa -- Principal" 0 0 0 'Nombre' "con cebolla" 'Servidor' "sin cebolla" 3 "con piminetos"
 exit 0
 # exit 0 indica que todo terminó bien
 # este comentario se para ver si funciona
