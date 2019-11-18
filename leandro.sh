ba4af8f7c77f7221493ba9916a6323c19b1f27e2
petanco <57901348+petanco@users.noreply.github.com>
2019-11-18 14:12:47 +0100

    Update leandro.sh

--- leandro.sh
+++ leandro.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #DESCRIPTION script basico
 #AUTHOR leandro galipolo
-echo "hola mundo"
-read a
-echo $a
-exit 0 
+dialog --title "Hola" --msgbox 'Hola Sololinux!' 6 20
+exit 0
 # exit 0 indica que todo terminó bien
 # este comentario se para ver si funciona
