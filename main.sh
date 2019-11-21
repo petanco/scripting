4eebb72efda4239be4ec9ea4bf144d1256f90f6c
petanco <57901348+petanco@users.noreply.github.com>
2019-11-21 12:15:34 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Declarar variables que se van a pedir al usuario
+  let admin
+  let pre_dominio
+  let post_dominio
+  let nom_csv
+#
+# Ahora pedimos al usuario los datos
+read -p "Escriba el nombre del administrador: " admin
+read -p "Escriba el nombre del dominio: " pre_dominio
+read -p "Escriba la extensión del dominio: " post_dominio
+read -p "Escriba el nombre del fichero CSV: " nom_csv
+#
