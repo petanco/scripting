4b4a6c6212845256162eb7772a14eaf7b7390727
root <root@localhost.localdomain>
2019-11-25 15:20:35 -0500

    Deleted some empty spaces

--- main.sh
+++ main.sh
@@ -13,7 +13,7 @@ read -p "Escriba la extensión del dominio: " post_dominio
 read -p "Escriba el nombre del fichero CSV: " nom_csv
 #
 
-# METEMOS EN LA VARIABLE number_uid_last EL VALOR DEL ULTIMO uidNumber para usarlo mas adelante 
+# METEMOS EN LA VARIABLE number_uid_last EL VALOR DEL ULTIMO uidNumber para usarlo mas adelante
 mkdir /tmp/parseador_ldif
 ldapsearch -H ldap://vitoria.gasteiz -x -LLL -b "dc=vitoria,dc=gasteiz" "(objectClass=posixAccount)" uidNumber > /tmp/parseador_ldif/uid_number_full
 sed -i '/^$/d' /tmp/parseador_ldif/uid_number_full #borrar lineas en blanco
@@ -30,18 +30,18 @@ IFS=';'
 while read num usuario unidad_organizativa descripcion
       do
         password=$(slappasswd -h {SHA} -s "$usuario")
-        echo "dn: uid=$usuario,ou=$unidad_organizativa,dc=$pre_dominio,dc=$post_dominio" >> /tmp/parseador_ldif/script_addUsers.ldif        
-        echo "objectClass: inetOrgPerson" >> /tmp/parseador_ldif/script_addUsers.ldif       
-        echo "objectClass: posixAccount" >> /tmp/parseador_ldif/script_addUsers.ldif        
-        echo "objectClass: shadowAccount" >> /tmp/parseador_ldif/script_addUsers.ldif       
-        echo "cn: $usuario" >> /tmp/parseador_ldif/script_addUsers.ldif       
-        echo "sn: $usuario" >> /tmp/parseador_ldif/script_addUsers.ldif        
-        echo "uid: $usuario" >> /tmp/parseador_ldif/script_addUsers.ldif        
-        echo "uidNumber: $number_uid_last" >> /tmp/parseador_ldif/script_addUsers.ldif        
-        echo "gidNumber: 1" >> /tmp/parseador_ldif/script_addUsers.ldif        
-        echo "userPassword: $password" >> /tmp/parseador_ldif/script_addUsers.ldif        
-        echo "homeDirectory: /home/$usuario" >> /tmp/parseador_ldif/script_addUsers.ldif     
-        echo "loginShell: /bin/bash" >> /tmp/parseador_ldif/script_addUsers.ldif        
+        echo "dn: uid=$usuario,ou=$unidad_organizativa,dc=$pre_dominio,dc=$post_dominio" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "objectClass: inetOrgPerson" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "objectClass: posixAccount" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "objectClass: shadowAccount" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "cn: $usuario" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "sn: $usuario" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "uid: $usuario" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "uidNumber: $number_uid_last" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "gidNumber: 1" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "userPassword: $password" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "homeDirectory: /home/$usuario" >> /tmp/parseador_ldif/script_addUsers.ldif
+        echo "loginShell: /bin/bash" >> /tmp/parseador_ldif/script_addUsers.ldif
         echo "gecos: $usuario" >> /tmp/parseador_ldif/script_addUsers.ldif
         echo "description: $descripcion" >> /tmp/parseador_ldif/script_addUsers.ldif
         printf "\n" >> /tmp/parseador_ldif/script_addUsers.ldif
