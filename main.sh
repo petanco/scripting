2287ab94229ae222a732755c6fec85244df6a157
petanco <57901348+petanco@users.noreply.github.com>
2019-11-25 12:55:29 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,4 +11,33 @@ read -p "Escriba el nombre del dominio: " pre_dominio
 read -p "Escriba la extensión del dominio: " post_dominio
 read -p "Escriba el nombre del fichero CSV: " nom_csv
 #
+INPUT = nom_csv
+
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read num usuario unidad_organizativa
+      do
+        ################
+      done < $INPUT
+IFS=$OLDIFS
+
+
+dn: uid=$usuario, ou=$unidad_organizativa,dc=$pre_dominio,dc=$post_dominio
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+cn: $usuario
+sn: $usuario
+uid: $usuario
+uidNumber: $num_user
+gidNumber: $group_number
+userPassword: $usuario
+homeDirectory: /home/$usuario
+loginShell: /bin/bash
+gecos: $usuario
+description: User account
+
+
+
 exit 0
