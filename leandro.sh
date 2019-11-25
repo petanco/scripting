f9b13b6db06aea931787f7ed6df20fff2cfd3776
petanco <57901348+petanco@users.noreply.github.com>
2019-11-25 13:04:28 +0100

    Update leandro.sh

--- leandro.sh
+++ leandro.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 #DESCRIPTION script basico
 #AUTHOR leandro galipolo
-dialog --menu "Programa -- Principal" 0 0 0 'Nombre' "con cebolla" 'Servidor' "sin cebolla" 3 "con piminetos"
+#dialog --menu "Programa -- Principal" 0 0 0 'Nombre' "con cebolla" 'Servidor' "sin cebolla" 3 "con piminetos"
+let usuario
+let o_u
+let uid
+
+$uid = 0
+
+while [ $uid -lt 10 ]
+      DO
+          echo "dn: uid=$usuario, ou=$o_u,dc=pre_dominio,dc=post_dominio" > anadir_usuarios.ldif
+          printf "\n" >> anadir_usuarios.ldif
+          echo "cn: $usuario"  >> anadir_usuarios.ldif
+          printf "\n" >> anadir_usuarios.ldif
+          echo "sn: $usuario"  >> anadir_usuarios.ldif
+          printf "\n" >> anadir_usuarios.ldif
+          echo "uid: $uid"  >> anadir_usuarios.ldif
+          printf "\n" >> anadir_usuarios.ldif
+          $uid=$uid+1
+      DONE
 exit 0
 # exit 0 indica que todo terminó bien
 # este comentario se para ver si funciona
