7f15f391cde9fbcf5c1bd33325d4dd2731c7c17e
petanco <57901348+petanco@users.noreply.github.com>
2019-11-26 16:17:06 +0100

    Update main_graphic.sh

--- main_graphic.sh
+++ main_graphic.sh
@@ -141,17 +141,13 @@ function continuar(){
 							--backtitle "$backtitle" \
 							--exit-label "Atrás" \
 							--textbox /tmp/parseador_ldif.$$/first_last_entries 40 70
-							answer_option=$?
-						if  [ $answer_option -eq 0 ] 
-							then
-								#ldapadd -x -D cn=$vAdmin,dc=$vDominio,dc=$vExtension -W -f /tmp/parseador_ldif.$$/script_addUsers.ldif
-								slapcat > /tmp/parseador_ldif.$$/slpcat_check.$$								
-								dialog  --clear \
-									--title "[ L A S T - C H E C K ]" \
-									--backtitle "$backtitle" \
-									--exit-label "Salir" \
-									--textbox $(tail -44 /tmp/parseador_ldif.$$/slpcat_check.$$) 40 70
-						fi
+						#ldapadd -x -D cn=$vAdmin,dc=$vDominio,dc=$vExtension -W -f /tmp/parseador_ldif.$$/script_addUsers.ldif
+						$(slapcat > /tmp/parseador_ldif.$$/slpcat_check.$$)
+						dialog  --clear \
+							--title "[ L A S T - C H E C K ]" \
+							--backtitle "$backtitle" \
+							--exit-label "Salir" \
+							--textbox $(tail -44 /tmp/parseador_ldif.$$/slpcat_check.$$) 40 70						
 				fi
 		fi
 	fi
