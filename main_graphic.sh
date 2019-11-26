235fcaab4c990ecf69f5e5cb767653aef05274fa
petanco <57901348+petanco@users.noreply.github.com>
2019-11-26 13:23:04 +0100

    Update main_graphic.sh

--- main_graphic.sh
+++ main_graphic.sh
@@ -9,28 +9,58 @@ let vAdmin
 let vDominio
 let vExtension
 let vCSV
+backtitle="Programa parseador"
 
 # delete temp files if program closes
 trap "rm -dr /tmp/parseador_ldif*; exit" SIGHUP SIGINT SIGTERM
 
 # functions declare
 
-function show_input(){
-	dialog --title "[ D O M I N I O ]" \
-	--backtitle "Programa parseador" \
-	--inputbox "Escrbia el nombre del administrador del dominio " 8 60 2>$vAdmin
+function show_inputAdmin(){
+	dialog --title "[ A D M I N ]" \
+	--backtitle $backtitle \
+	--inputbox "Escriba el nombre del administrador del dominio " 8 60 2>$OUTPUT
+	vAdmin=$(cat $OUTPUT)
 }
+function show_inputDominio(){
+	dialog --title "[ D O M I N I O ]" \
+	--backtitle $backtitle \
+	--inputbox "Escriba el nombre del administrador del dominio " 8 60 2>$OUTPUT
+	vDominio=$(cat $OUTPUT)
+}
+function show_inputExtension(){
+	dialog --title "[ E X T E N S I O N ]" \
+	--backtitle $backtitle \
+	--inputbox "Escriba el nombre del administrador del dominio " 8 60 2>$OUTPUT
+	vExtension=$(cat $OUTPUT)
+}
+function show_inputCSV(){
+	dialog	--title "[-- C S V --]" \
+		--backtitle "$backtitle" \		
+		--fselect $HOME/ 14 48 \ 2>$OUTPUT
+        vCSV=$(cat $OUTPUT
+}
+function continuar(){
+	dialog  --clear \
+		--title "[-- I N F O --]" \
+		--backtitle "$backtitle" \
+		--ok-label "Crear LDIF" \
+		--extra-button \
+		--extra-label "Cancelar" \
+		--msgbox "$SCRIPT_INFO" 10 40
+}
+
 # while menu dialog
 HEIGHT=0
 WIDTH=0
 
 # loop for menu
 while true; do
-	dialog --backtitle "Programa parseador" \
+	dialog --backtitle $backtitle \
 	--title "[ M E N U ]" \
 	--menu "Seleccione las siguientes opciones:"	$HEIGHT $WIDTH 6 \
 	Admin "Indique nombre del administrador el dominio" \
-	Servidor "Indique nombre del servidor" \
+	Dominio "Indique nombre del servidor" \
 	Extension "Indique nombre de la extensión del dominio" \
 	CSV "Indique la ubicación del fichero .csv con los datos" \
 	Continuar "Si ha rellenado todo, click aqui" \
@@ -40,14 +70,13 @@ while true; do
 
   case $selection in
     Admin)
-	show_input;;
-    Servidor)
-	show_input;;
+	show_inputAdmin;;
+    Dominio)
+	show_inputDominio;;
     Extension)
-	show_input;;
+	show_inputExtension;;
     CSV)
-	
-      ;;
+    	show_inputCSV;;
     Continuar)
 	
       ;;
