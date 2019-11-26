6fec0f1a8d6043f8457e0552ef3ad40ae9e35343
petanco <57901348+petanco@users.noreply.github.com>
2019-11-26 16:38:46 +0100

    Update main_graphic.sh

--- main_graphic.sh
+++ main_graphic.sh
@@ -51,11 +51,54 @@ function show_inputCSV(){
 	ITEM="CSV"
 }
 
-function show_inputPassword(){
-	dialog --title "[ P A S S W O R D ]" \
-	--backtitle "$backtitle" \
-	--inputbox "Escriba el nombre del administrador del dominio " 8 60 2>$INPUT
-	vAdmin=$(cat $INPUT)
+function getLast(){
+	ldapsearch -H ldap://$vDominio.$vExtension -x -LLL -b "dc=$vDominio,dc=$vExtension" "(objectClass=posixAccount)" uidNumber > /tmp/parseador_ldif.$$/uid_number_full.$$
+	sed -i '/^$/d' /tmp/parseador_ldif.$$/uid_number_full.$$ #borrar lineas en blanco
+	tail -1 /tmp/parseador_ldif.$$/uid_number_full.$$ | cut -d' ' -f2- > /tmp/parseador_ldif.$$/uid_number_alone.$$ #borrar primera palabra
+	number_uid_last=$(cat /tmp/parseador_ldif.$$/uid_number_alone.$$)
+	((number_uid_last=$number_uid_last+1))
+}
+
+function ldip_loop(){
+	IFS=';'
+	[ ! -f $vCSV ] && { echo "$vCSV file not found"; exit 99; }
+	while read num usuario unidad_organizativa descripcion
+	      do
+		password=$(slappasswd -h {SHA} -s "$usuario")
+		echo "dn: uid=$usuario,ou=$unidad_organizativa,dc=$vDominio,dc=$vExtension" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "objectClass: inetOrgPerson" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "objectClass: posixAccount" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "objectClass: shadowAccount" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "cn: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "sn: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "uid: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "uidNumber: $number_uid_last" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "gidNumber: 1" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "userPassword: $password" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "homeDirectory: /home/$usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "loginShell: /bin/bash" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "gecos: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		echo "description: $descripcion" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		printf "\n" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
+		((number_uid_last=$number_uid_last+1))
+	      done <$vCSV
+}
+
+function ldifShowFL(){
+	$(cp /tmp/parseador_ldif.$$/script_addUsers.ldif /tmp/parseador_ldif.$$/first_last_entries_Before) # Copy to avoid using .ldif
+	sed -i '/^$/d' /tmp/parseador_ldif.$$/first_last_entries_Before # Delete empty spaces or lines
+	echo "[PRIMERA ENTRADA DEL .ldif]" >> /tmp/parseador_ldif.$$/first_last_entries
+	$(head -14 /tmp/parseador_ldif.$$/first_last_entries_Before >> /tmp/parseador_ldif.$$/first_last_entries) # Show first entry
+	echo "" >> /tmp/parseador_ldif.$$/first_last_entries
+	echo "[ULTIMA ENTRADA DEL .ldif]" >> /tmp/parseador_ldif.$$/first_last_entries
+	last_entryLDIF=$(tail -14 /tmp/parseador_ldif.$$/first_last_entries_Before >> /tmp/parseador_ldif.$$/first_last_entries) # Show last entry
+	echo "" >> /tmp/parseador_ldif.$$/first_last_entries
+	echo "[ENTRADAS TOTALES DEL .ldif]" >> /tmp/parseador_ldif.$$/first_last_entries
+	dialog  --clear \
+		--title "[ L D I F ]" \
+		--backtitle "$backtitle" \
+		--exit-label "Atrás" \
+		--textbox /tmp/parseador_ldif.$$/first_last_entries 40 70
 }
 
 function continuar(){
@@ -90,64 +133,28 @@ function continuar(){
 				answer_option=$?
 				if  [ $answer_option -eq 0 ]
 					then
-						# get last uidNumber of ldap to start from that one
-						ldapsearch -H ldap://$vDominio.$vExtension -x -LLL -b "dc=$vDominio,dc=$vExtension" "(objectClass=posixAccount)" uidNumber > /tmp/parseador_ldif.$$/uid_number_full.$$
-						sed -i '/^$/d' /tmp/parseador_ldif.$$/uid_number_full.$$ #borrar lineas en blanco
-						tail -1 /tmp/parseador_ldif.$$/uid_number_full.$$ | cut -d' ' -f2- > /tmp/parseador_ldif.$$/uid_number_alone.$$ #borrar primera palabra
-						number_uid_last=$(cat /tmp/parseador_ldif.$$/uid_number_alone.$$)
-						((number_uid_last=$number_uid_last+1))
-						# end_last_uidNumber
-
-						# loop to create .ldif
-						IFS=';'
-						[ ! -f $vCSV ] && { echo "$vCSV file not found"; exit 99; }
-						while read num usuario unidad_organizativa descripcion
-						      do
-							password=$(slappasswd -h {SHA} -s "$usuario")
-							echo "dn: uid=$usuario,ou=$unidad_organizativa,dc=$vDominio,dc=$vExtension" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "objectClass: inetOrgPerson" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "objectClass: posixAccount" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "objectClass: shadowAccount" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "cn: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "sn: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "uid: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "uidNumber: $number_uid_last" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "gidNumber: 1" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "userPassword: $password" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "homeDirectory: /home/$usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "loginShell: /bin/bash" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "gecos: $usuario" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							echo "description: $descripcion" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							printf "\n" >> /tmp/parseador_ldif.$$/script_addUsers.ldif
-							((number_uid_last=$number_uid_last+1))
-						      done <$vCSV
-						# loop end
-						# show first & last ldif entries
-							$(cp /tmp/parseador_ldif.$$/script_addUsers.ldif /tmp/parseador_ldif.$$/first_last_entries_Before)
-							sed -i '/^$/d' /tmp/parseador_ldif.$$/first_last_entries_Before
-							echo "[PRIMERA ENTRADA DEL .ldif]" >> /tmp/parseador_ldif.$$/first_last_entries
-							$(head -14 /tmp/parseador_ldif.$$/first_last_entries_Before >> /tmp/parseador_ldif.$$/first_last_entries)
-							echo "" >> /tmp/parseador_ldif.$$/first_last_entries
-							echo "[ULTIMA ENTRADA DEL .ldif]" >> /tmp/parseador_ldif.$$/first_last_entries
-							last_entryLDIF=$(tail -14 /tmp/parseador_ldif.$$/first_last_entries_Before >> /tmp/parseador_ldif.$$/first_last_entries)
-							echo "" >> /tmp/parseador_ldif.$$/first_last_entries
-							echo "[ENTRADAS TOTALES DEL .ldif]" >> /tmp/parseador_ldif.$$/first_last_entries
-							# count entries to be added
-								$(grep -c ^$ /tmp/parseador_ldif.$$/script_addUsers.ldif >> /tmp/parseador_ldif.$$/first_last_entries)
-							#end count
-						#end show first & last
-						dialog  --clear \
-							--title "[ L D I F ]" \
-							--backtitle "$backtitle" \
-							--exit-label "Atrás" \
-							--textbox /tmp/parseador_ldif.$$/first_last_entries 40 70
+						# Get last uidNumber to begin adding from that one
+						getLast
+						
+						# Loop to create .ldif
+						ldif_loop
+						
+						# Show first & last ldif entries
+						ldifShowFL
+						
+						# Total entries in .ldif to be added
+						$(grep -c ^$ /tmp/parseador_ldif.$$/script_addUsers.ldif >> /tmp/parseador_ldif.$$/first_last_entries)
+												
+						# Add entries to LDAP
 						#ldapadd -x -D cn=$vAdmin,dc=$vDominio,dc=$vExtension -W -f /tmp/parseador_ldif.$$/script_addUsers.ldif
-						slapcat > /tmp/parseador_ldif.$$/slpcat_check.$$.txt
+						
+						# Show last two added LDAP users
+						ldap_check=$(slapcat | tail -44)
 						dialog  --clear \
 							--title "[ L A S T - C H E C K ]" \
 							--backtitle "$backtitle" \
 							--exit-label "Salir" \
-							--textbox $(tail -44 /tmp/parseador_ldif.$$/slpcat_check.$$.txt) 40 70
+							--textbox $ldap_check 40 70
 				fi
 		fi
 	fi
